2f7d42fdc464628725539d7ef31cab4983861d29
Monica  Karasek <mlkarasek@gmail.com>
2017-10-28 19:15:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,19 +6,11 @@ class Song
   extend Findable::ClassMethods
   include Memorable::InstanceMethods
   include Paramable::InstanceMethods
-  
+
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
-
-  def initialize
-    @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
 
   def artist=(artist)
     @artist = artist
