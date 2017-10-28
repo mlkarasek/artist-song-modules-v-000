34ddd6d346077e3d4efee65099c3a28ad04a13ae
Monica  Karasek <mlkarasek@gmail.com>
2017-10-28 19:16:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,6 +12,14 @@ class Song
 
   @@songs = []
 
+#  def initialize
+  #  @@songs << self
+  #end
+
+#  def self.find_by_name(name)
+#    @@songs.detect{|a| a.name == name}
+#  end
+
   def artist=(artist)
     @artist = artist
   end
@@ -20,9 +28,6 @@ class Song
     #@@songs.clear
   #end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 
   #def self.count
     #self.all.count
